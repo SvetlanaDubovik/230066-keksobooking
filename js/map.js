8b423ca6b1ec7546f6d73d0e523b1fb104bb09e2
SvetlanaDubovik <voronina.s.u@gmail.com>
2017-08-27 18:39:13 +0500

    Исправила оформление

--- js/map.js
+++ js/map.js
@@ -232,20 +232,18 @@ offerDialog.addEventListener('click', function (evt) {
 var timeIn = document.querySelector('#timein');
 var timeOut = document.querySelector('#timeout');
 
-var doSameSelectValue = function(select1, select2) {
+var doSameSelectValue = function (select1, select2) {
   var selectOption = select1.options.selectedIndex;
   select2.options.selectedIndex = selectOption;
 };
 
-timeIn.addEventListener('change', function() {
+timeIn.addEventListener('change', function () {
   doSameSelectValue(timeIn, timeOut);
 });
 
-timeOut.addEventListener('change', function() {
-    doSameSelectValue(timeOut, timeIn);
+timeOut.addEventListener('change', function () {
+  doSameSelectValue(timeOut, timeIn);
 });
-
-
 
 var roomNumber = document.querySelector('#room_number');
 var roomNumberOptions = roomNumber.options;
@@ -253,41 +251,41 @@ var capacity = document.querySelector('#capacity');
 var capacityOptions = capacity.options;
 var capacityOptionsLength = capacityOptions.length;
 
-var removeDisabledAttribute = function() {
+var removeDisabledAttribute = function () {
   for (var i = 0; i < capacityOptionsLength; i++) {
     capacityOptions[i].removeAttribute('disabled');
   }
 };
 
-var isCorrespondRoomToCapacity = function() {
-switch(roomNumberOptions.selectedIndex){
+var isCorrespondRoomToCapacity = function () {
+  switch (roomNumberOptions.selectedIndex){
     // 1 комната
     case 0:
-    removeDisabledAttribute();
-    for (var i = 0; i < capacityOptionsLength; i++ ) {
-      if (capacityOptions[i].value !== '1') {
+     removeDisabledAttribute();
+     for (var i = 0; i < capacityOptionsLength; i++) {
+       if (capacityOptions[i].value !== '1') {
         capacityOptions[i].setAttribute('disabled', 'disabled');
-      }
-    }
+       }
+     }
     capacityOptions.selectedIndex = 2;
     break;
-    // 2 комнаты
+  // 2 комнаты
   case 1:
     removeDisabledAttribute();
-    for (var i = 0; i < capacityOptionsLength; i++ ) {
+    for (var i = 0; i < capacityOptionsLength; i++) {
       if (capacityOptions[i].value > 2 || capacityOptions[i].value === '0') {
         capacityOptions[i].setAttribute('disabled', 'disabled');
       }
     }
     capacityOptions.selectedIndex = 2;
     break;
-    // 3 комнаты
+  // 3 комнаты
   case 2:
     removeDisabledAttribute();
     capacityOptions[3].setAttribute('disabled', 'disabled');
     capacityOptions.selectedIndex = 2;
-    break;  
-    // 100 комнат
+    break;
+  // 100 комнат
   case 3:
     removeDisabledAttribute();
     for (var i = 0; i < capacityOptionsLength; i++ ) {
@@ -295,16 +293,16 @@ switch(roomNumberOptions.selectedIndex){
         capacityOptions[i].setAttribute('disabled', 'disabled');
       }
     }
-     capacityOptions.selectedIndex = 3;
+    capacityOptions.selectedIndex = 3;
     break;
   }
 };
 
-roomNumber.addEventListener('change', function() {
+roomNumber.addEventListener('change', function () {
   isCorrespondRoomToCapacity();
 });
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   isCorrespondRoomToCapacity();
 });
 
@@ -312,33 +310,33 @@ var houseType = document.querySelector('#type');
 var houseTypeOptions = houseType.options;
 var price = document.querySelector('#price');
 
-var checkMinPrice = function(value) {
-  if (value > 1000) {
+var checkMinPrice = function (value) {
+  if (+value > 1000) {
     price.setAttribute('value', value);
-  } 
+  }
 };
 
-var isCorrespondTypeToPrice = function() {
-  switch(houseTypeOptions.selectedIndex) {
+var isCorrespondTypeToPrice = function () {
+  switch (houseTypeOptions.selectedIndex) {
     case 0:
-      price.setAttribute('min', '1000');
-      checkMinPrice(1000);
+      price.setAttribute('min', MIN_PRICE[1]);
+      checkMinPrice(MIN_PRICE[1]);
       break;
     case 1:
-      price.setAttribute('min', '0');
-      checkMinPrice(0);
+      price.setAttribute('min', MIN_PRICE[0]);
+      checkMinPrice(MIN_PRICE[0]);
       break;
     case 2:
-      price.setAttribute('min', '5000');
-      checkMinPrice(5000);
+      price.setAttribute('min', MIN_PRICE[2]);
+      checkMinPrice(MIN_PRICE[2]);
       break;
     case 3:
-      price.setAttribute('min', '10000');
-      checkMinPrice(10000);
+      price.setAttribute('min', MIN_PRICE[3]);
+      checkMinPrice(MIN_PRICE[3]);
       break;
   }
 };
 
-houseType.addEventListener('change', function() {
+houseType.addEventListener('change', function () {
   isCorrespondTypeToPrice();
 });
